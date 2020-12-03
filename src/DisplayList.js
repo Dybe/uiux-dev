2b485bbc9c21257f1f4d863af614ae07d26bb575
Dybe <33331120+Dybe@users.noreply.github.com>
2020-12-03 03:07:14 -0500

    Update DisplayList.js

--- src/DisplayList.js
+++ src/DisplayList.js
@@ -4,25 +4,6 @@ import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert'
 import { useState } from "react";
 
-// function AlertDismissibleExample() {
-//     const [show, setShow] = useState(true);
-
-//     if (show) {
-//       return (
-//         <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-//           <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-//           <p>
-//             Change this and that and try again. Duis mollis, est non commodo
-//             luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-//             Cras mattis consectetur purus sit amet fermentum.
-//           </p>
-//         </Alert>
-//       );
-//     }
-//     return <Button onClick={() => setShow(true)}>Show Alert</Button>;
-//   }
-
-
 class DisplayList extends React.Component {
     constructor(props) {
         super(props)
@@ -104,7 +85,6 @@ class DisplayList extends React.Component {
                         </div>
 
                     </div>
-
 
                 )}
 
